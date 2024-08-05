d35f0e0fd9312f8ff73d547a5fd894c016069ea4
kibochamark <mark.varu@student.cuk.ac.ke>
2024-08-05 20:29:23 +0300

    update on  signup link

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -28,7 +28,7 @@ function page() {
 
           <div className="mt-4 text-center text-sm">
             Don&apos;t have an account?{" "}
-            <Link href="/auth/signup" className="underline">
+            <Link href="/signup" className="underline">
               Sign up
             </Link>
           </div>

c65098fcc1bbcb747ee88eeb0e50d1554c89cc7b
kibochamark <mark.varu@student.cuk.ac.ke>
2024-08-05 22:09:13 +0300

    update on messages

--- components/Signup/Clientsignup.tsx
+++ components/Signup/Clientsignup.tsx
@@ -47,7 +47,7 @@ const Clientsignup = () => {
                     toast.error("unable to create user, try again later")
                 }
             } catch (e: any) {
-                toast.error("something went wrong")
+                toast.error("something went wrong or user already exists")
 
             }
 
